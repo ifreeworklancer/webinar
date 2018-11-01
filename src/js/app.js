80f1624d26ddc9ab9c982d4888987314c3e30fd6
ifreeworklancer <ifreeworklancer@gmail.com>
2018-11-01 18:23:25 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+import jquery from 'jquery';
+import ScrollReveal from 'scrollreveal'
+
+window.jQuery = window.$ = jquery;
+
+
+
+(function () {
+    let isCounted = 0;
+    /**
+     * Burger-menu
+     */
+    $('.burger-menu').click(function () {
+        var menu = $('.menu');
+        $(this).toggleClass('active');
+        if (menu.is(':visible')) {
+            menu.slideUp();
+        } else {
+            menu.slideDown().css('display', 'flex');
+        }
+    });
+
+
+    /**
+     * Form-label
+     */
+
+    $('.form-control').on('focus', function () {
+        $(this).parent().addClass('in-focus');
+    });
+
+    $('.form-control').on('blur', function () {
+        if ($(this).val() !== "") {
+            $(this).parent().addClass('in-focus');
+        } else {
+            $(this).parent().removeClass('in-focus');
+        }
+    });
+
+
+})(jQuery)
